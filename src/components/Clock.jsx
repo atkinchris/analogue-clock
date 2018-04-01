995616253cdc8b3d8173c181578b01baf3d02bba
Chris Atkin <atkinchris@gmail.com>
2018-04-01 14:08:23 +0100

    Improve hand styling

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -4,8 +4,14 @@ import PropTypes from 'prop-types'
 const Clock = ({ rotations }) => (
   <svg width="100" height="100">
     <circle cx="50" cy="50" r="45" className="frame" />
-    <line x1="50" y1="50" x2="50" y2="15" className="hand" transform={`rotate(${rotations[0]} 50 50)`} />
-    <line x1="50" y1="50" x2="50" y2="15" className="hand" transform={`rotate(${-rotations[1]} 50 50)`} />
+    <g transform={`rotate(${rotations[0]} 50 50)`}>
+      <line x1="50" y1="40" x2="50" y2="15" strokeLinecap="square" className="hand" />
+      <line x1="50" y1="50" x2="50" y2="25" strokeLinecap="round" className="hand" />
+    </g>
+    <g transform={`rotate(${-rotations[1]} 50 50)`}>
+      <line x1="50" y1="40" x2="50" y2="15" strokeLinecap="square" className="hand" />
+      <line x1="50" y1="50" x2="50" y2="25" strokeLinecap="round" className="hand" />
+    </g>
   </svg>
 )
 
